079522b596686d069a56da2c2fcf3708d6623b4e
akeenzweb <akinboladesalako@gmail.com>
2025-06-20 18:07:16 +0100

    Fixed the Large screen Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,17 +19,20 @@ export default function Header() {
     <div >
       {/* For Laptops */}
       <div className={`${style.header} hidden lg:block fixed top-0 left-0 w-full h-28 z-10`}>
-        <div className={`${style.blurOverlay}  lg:flex items-center justify-center  w-full h-28 z-10 px-4 md:px-20 `}>
-            <div className={` flex justify-between items-center w-full`}>
+        {/* <div className={`${style.blurOverlay}  lg:flex items-center justify-center  w-full h-28 z-10 px-4 md:px-20 `}> */}
+            <div className={`absolute top-0 left-0 flex justify-between items-center w-full z-[9999] px-4 lg:px-20`}>
                 <img src={Logo} alt="" />
                 <ul className={`${style.dmSans} flex text-[#787878] tracking-[6px] font-semibold`}>
-                    <li>Projects</li>
+                    <li onClick={() => alert('hey')}>Projects</li>
                     <li className='ml-16'>About Me</li>
                     <li className='ml-16'>Contacts</li>
                     <li className='ml-16'>Projects</li>
                 </ul>
             </div>
-        </div>
+            <div className={`${style.blurOverlay} absolute top-0 left-0 lg:flex items-center justify-center  w-full h-28 z-[-9999] px-4 md:px-20 `}>
+
+            </div>
+        {/* </div> */}
       </div>
       
 
