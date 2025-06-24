e69b3e0253237957ca2c5fef6f5ccf4498f7a728
akeenzweb <akinboladesalako@gmail.com>
2025-06-24 19:48:59 +0100

    fixing sec2 array

--- src/components/HomeComponents/Section2/Section2.jsx
+++ src/components/HomeComponents/Section2/Section2.jsx
@@ -45,28 +45,54 @@ export default function Home() {
       }, []);
       
 
-      const items = [project1, project2, project3, project4, project5, project6];
+      const items = [
+        {
+          name: 'IAMBUSAYO BRAND WEBSITE',
+          coverImage: project1,
+        },
+        {
+          name: 'SHOPKUZA FASHION WEBSITE',
+          coverImage: project2,
+        },
+        {
+          name: 'QA ALL IN ONE SOFTWARE',
+          coverImage: project3,
+        },
+        {
+          name: 'LIVE MUSIC CONCERT',
+          coverImage: project4,
+        },
+        {
+          name: 'ZENDEE DATING APP',
+          coverImage: project5,
+        },
+        {
+          name: 'CHURCH POCKET APP',
+          coverImage: project6,
+        },
+      ];
 
 
   return (
     // <div className="relative z-20 mt-[100vh]">
         // <div className={style.section}> 
-    <div className={`${style.rippleBackground} h-[2000px] lg:h-[1400px] z-0 bg-slate-500 px-4 lg:px-20 py-12 lg:py-32`} ref={rippleRef}>
+    <div className={`${style.rippleBackground} h-[2200px] lg:h-[1500px] z-0 px-4 lg:px-20 py-12 lg:py-32`} ref={rippleRef}>
         {/* <div > */}
           <h1 className={`${style.dmSans} mb-20 lg:mb-48 text-white text-center font-semibold text-xl lg:text-2xl tracking-[6px]`}>MY PROJECTS</h1>
-          <div className={`${style.dmSans}w-full grid grid-cols-1 md:grid-cols-3 gap-x-16 gap-y-16 lg:gap-x-16 lg:gap-y-32 `}>
+          <div className={`${style.dmSans}w-full grid grid-cols-1 md:grid-cols-3 gap-x-16 gap-y-16 lg:gap-x-16 lg:gap-y-60 `}>
               {items.map((item, index) => {
                   const isMiddleColumn = index % 3 === 1;
 
                   return (
                     <div
                       key={index}
-                      className={` ${isMiddleColumn ? 'lg:-mt-32' : ''}`}
+                      className={` ${isMiddleColumn ? 'lg:-mt-24' : ''}`}
                     >
                       
                         <div className="relative">
-                          <h1 style={{ WebkitTextStroke: '1.22px white', textShadow: '1px 1px 2px rgba(0,0,0,0.25)' }} className="drop-shadow-md text-white text-5xl lg:text-7xl font-bold absolute -top-8 lg:-top-10 right-1 ">01</h1>
-                          <img className="w-full h-full object-cover" src={item} alt="" />
+                          <h1 style={{ WebkitTextStroke: '1.22px white', textShadow: '1px 1px 2px rgba(0,0,0,0.25)' }} className="drop-shadow-md text-white text-5xl lg:text-7xl font-bold absolute -top-8 lg:-top-10 right-1 ">0{index + 1}</h1>
+                          <img className="w-full h-full object-cover" src={item.coverImage} alt="" />
+                          <h1 className="text-[#AAAAAA] tracking-[3px] mt-4 text-lg">{item.name}</h1>
                         </div>
                       
                     </div>
