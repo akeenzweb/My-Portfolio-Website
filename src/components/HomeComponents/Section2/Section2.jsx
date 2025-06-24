583fac34a977ef32cbd7e4ee82906baf5e373be8
akeenzweb <akinboladesalako@gmail.com>
2025-06-24 15:15:43 +0100

    fixing sec2 height deploy 2i

--- src/components/HomeComponents/Section2/Section2.jsx
+++ src/components/HomeComponents/Section2/Section2.jsx
@@ -48,10 +48,31 @@ export default function Home() {
 
   return (
     // <div className="relative z-20 mt-[100vh]">
+        // <div className={style.section}> 
     <div className="z-0">
-        <div>
-        {/* <div className={style.section}> */}
-            <div className={`${style.rippleBackground} ${style.redB} px-4 lg:px-20 py-12 lg:py-32`} ref={rippleRef}>
+        <div className={`${style.rippleBackground} bg-slate-500 px-4 lg:px-20 py-12 lg:py-32`} ref={rippleRef}>
+          <h1 className={`${style.dmSans} mb-20 lg:mb-48 text-white text-center font-semibold text-xl lg:text-2xl tracking-[6px]`}>MY PROJECTS</h1>
+          <div className={`${style.dmSans}w-full grid grid-cols-1 md:grid-cols-3 gap-x-16 gap-y-16 lg:gap-x-16 lg:gap-y-32 `}>
+              {items.map((item, index) => {
+                  const isMiddleColumn = index % 3 === 1;
+
+                  return (
+                    <div
+                      key={index}
+                      className={` ${isMiddleColumn ? 'lg:-mt-32' : ''}`}
+                    >
+                      <div className=" ">
+                        <div className="relative h-full">
+                          <h1 style={{ WebkitTextStroke: '1.22px white', textShadow: '1px 1px 2px rgba(0,0,0,0.25)' }} className="drop-shadow-md text-white text-5xl lg:text-7xl font-bold absolute -top-8 lg:-top-10 right-1 ">01</h1>
+                          <img className="w-full h-full object-cover" src={item} alt="" />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+          </div>
+          <h3 className={`${style.dmSans} mt-20 lg:mt-48 text-[#AAAAAA] text-center font-semibold text-lg lg:text-xl tracking-[6px]`}>and many more...</h3>
+            {/* <div className={`${style.rippleBackground} ${style.redB} px-4 lg:px-20 py-12 lg:py-32`} ref={rippleRef}>
               <h1 className={`${style.dmSans} mb-20 lg:mb-48 text-white text-center font-semibold text-xl lg:text-2xl tracking-[6px]`}>MY PROJECTS</h1>
               
               <div className={`${style.dmSans}w-full grid grid-cols-1 md:grid-cols-3 gap-x-16 gap-y-16 lg:gap-x-16 lg:gap-y-32 `}>
@@ -77,7 +98,7 @@ export default function Home() {
 
                 <h3 className={`${style.dmSans} mt-20 lg:mt-48 text-[#AAAAAA] text-center font-semibold text-lg lg:text-xl tracking-[6px]`}>and many more...</h3>
 
-            </div>
+            </div> */}
         </div>
         
     </div>
