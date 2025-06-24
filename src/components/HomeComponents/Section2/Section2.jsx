f259e530dcc82e212454103e28c82ac6a91a4e30
akeenzweb <akinboladesalako@gmail.com>
2025-06-24 11:03:28 +0100

    Started Section 2

--- src/components/HomeComponents/Section2/Section2.jsx
+++ src/components/HomeComponents/Section2/Section2.jsx
@@ -4,6 +4,13 @@ import React, { useEffect, useRef } from "react";
 
 import style from './Section2.module.css'
 
+import project1 from '../../../assets/projects/project1.png'
+import project2 from '../../../assets/projects/project2.png'
+import project3 from '../../../assets/projects/project3.png'
+import project4 from '../../../assets/projects/project4.png'
+import project5 from '../../../assets/projects/project5.png'
+import project6 from '../../../assets/projects/project6.png'
+
 export default function Home() {
 
     const rippleRef = useRef();
@@ -34,15 +41,39 @@ export default function Home() {
           }
         };
       }, []);
+      
+
+      const items = [project1, project2, project3, project4, project5, project6];
 
 
   return (
     // <div className="relative z-20 mt-[100vh]">
-    <div className="relative z-0">
+    <div className="relative z-0 h-100">
         <div className={style.section}>
-            <div className={style.rippleBackground} ref={rippleRef}>
-                {/* <h1 className="text-3xl font-bold text-blue-600">Tailwind is Working 🎉</h1>
-                <p className="text-sm text-gray-600">If you can see this styled text, it’s all good.</p> */}
+            <div className={`${style.rippleBackground} px-4 lg:px-20 pt-12 lg:pt-32`} ref={rippleRef}>
+              <h1 className={`${style.dmSans} mb-10 lg:mb-48 text-white text-center font-semibold text-2xl tracking-[6px]`}>MY PROJECTS</h1>
+              
+              <div className={`${style.dmSans}w-full grid grid-cols-1 md:grid-cols-3 gap-x-16 gap-y-16 lg:gap-x-16 lg:gap-y-32 `}>
+                {items.map((item, index) => {
+                  const isMiddleColumn = index % 3 === 1;
+
+                  return (
+                    <div
+                      key={index}
+                      className={` ${isMiddleColumn ? 'lg:-mt-32' : ''}`}
+                    >
+                      <div className=" ">
+                        <div className="relative h-full">
+                          <h1 style={{ WebkitTextStroke: '1.22px white', textShadow: '1px 1px 2px rgba(0,0,0,0.25)' }} className="drop-shadow-md text-white text-7xl font-bold absolute -top-10 right-1 ">01</h1>
+                          <img className="w-full h-full object-cover" src={item} alt="" />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+                
+              </div>
+
 
             </div>
         </div>
