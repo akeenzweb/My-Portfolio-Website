91affa2fde71a7be2c4a914d530d40875a4049e3
akeenzweb <akinboladesalako@gmail.com>
2025-07-29 21:43:27 +0100

    working on projects ii

--- src/components/ProjectComponent/ProjectView.jsx
+++ src/components/ProjectComponent/ProjectView.jsx
@@ -4,6 +4,8 @@ import style from './ProjectView.module.css'
 
 import Cover1 from '../../assets/images/cover1.svg'
 import Bulb from '../../assets/images/bulb.svg'
+import Sketch1 from '../../assets/images/sketch1.png'
+import Wireframes1 from '../../assets/images/Wireframes1.svg'
 
 export default function ProjectView() {
   return (
@@ -32,12 +34,18 @@ export default function ProjectView() {
             </div>
 
             {/* THE APPROACH */}
-            <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 mt-20">
+            <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 mt-20 ">
                 <div className='col-span-3'>
                     <h1 className='text-2xl md:text-3xl font-semibold leading-10 md:leading-[52px] mb-4'>The Approach</h1>
                     <p className='text-base tracking-[1px] text-[#AAAAAA] leading-9'>The project began with initial discovery sessions, where I engaged the client in conversations to understand her tone, preferences, and vision. From there, I moved into the design thinking process, starting with quick sketch explorations and low-fidelity wireframes to define the structure and flow. This allowed us to experiment with layout ideas early and stay iterative. Feedback loops were constant—client input shaped every major design decision. With the sitemap and wireframes in place, I advanced into visual mockups and interactive prototypes, focusing on hierarchy, typography, and gentle micro-interactions that would eventually bring the site to life. Every step of the way, I remained in dialogue with Busayo, refining the interface based on both aesthetic goals and real usability concerns.</p>
                 </div>
-                <div className='col-span-2'>some text here</div>
+                <div className='col-span-2 '><img className='' src={Sketch1} alt="" /></div>
+            </div>
+
+            {/* WIREFRAMES */}
+            <div className='relative border-b border-[#444343] pb-20 mt-20'>
+                <h1 className='text-2xl md:text-3xl font-semibold leading-10 md:leading-[52px] mb-4'>Wireframes</h1>
+                <div className=''><img src={Wireframes1} alt="" /></div>
             </div>
 
         </div>
